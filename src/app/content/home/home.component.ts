315e94545e6e340df485c395be708a1eb6449356
John1316 <johnmaher179@gmail.com>
2022-03-26 03:18:50 +0200

    sort by child and parent commit

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import * as _ from 'lodash';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,27 +12,45 @@ export class HomeComponent implements OnInit {
   incrementSeconds:string ="s"
   sortedUsers : any[] =[]
   users:any[] =[]
+  userSorted:any[] =[]
+  userContainer:any[] =[]
+  yourObj:any[] =[]
   constructor(private _ApiService:ApiService) { }
   showUsersData(){
     this._ApiService.getUsersData().subscribe(
       (response) => {
-        this.users = response;
-
-        this.sortedUsers = _.sortBy(this.users,
+        // this.users = response;
+        this.users = _.sortBy(response,
           (item) => {
-              return +(item.code.replace(',', ''));
-          })
-          // slicing parent
-            console.log(this.sortedUsers[2].code);
-            console.log(this.sortedUsers[2].code.slice(-9,-5));
+          return +(item.code.replace(',', ''));
+        })
+            let table = [];
+            this.users.forEach(
+              ele =>
+              table[ele.code.split('.').pop()] = ele
 
+            );
+            this.yourObj = this.users.map(ele => {
+              let splitCode = ele.code.split('.');
+              ele.parent = splitCode[splitCode.length - 2];
+              // this.userContainer = ele
+              this.userContainer.push(ele)
+              this.userSorted = _.sortBy(this.userContainer,
+                (item) => {
+                return +(item.code.replace('.', ''));
+              })
+              console.log(this.userSorted);
+              // console.log(
+              //   this.userContainer
+              //   );
+            })
       }
     )
 
   }
 
   ngOnInit(): void {
-    this.showUsersData()
+    this.showUsersData();
   }
 
 }
